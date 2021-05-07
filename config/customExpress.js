15f4a372a7be43602e45877d7e93ba09b0c69b00
Victor Munhoz <vbm2906@gmail.com>
2021-05-07 11:53:53 -0300

    Adicionando conexao com banco de dados MySQL

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const consign = require('consign')
-const bodyParser = require('body-parser')
 
 module.exports = () => {
     const app = express()
 
-    app.use(bodyParser.urlencoded({extended: true}))
-    app.use(bodyParser.json())
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.json())
 
     consign()
         .include('controllers')
